4898f1932da4edb8c1c1badf85d0b3b3534f2c59
Dhruvin Sudani <sudanidhruvin@gmail.com>
2025-12-30 23:52:48 +0530

    - google setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7085320120847108"
+          strategy="beforeInteractive"
+          crossOrigin="anonymous"
+        />
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-7085320120847108"
+        ></meta>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
